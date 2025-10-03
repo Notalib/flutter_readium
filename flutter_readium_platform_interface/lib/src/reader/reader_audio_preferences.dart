df9fd17a0889b62f46f81500be8c691af229cba3
SifAa <43337067+SifAa@users.noreply.github.com>
2025-10-03 11:09:59 +0200

    feat: add control panel info type to audio preferences

--- flutter_readium_platform_interface/lib/src/reader/reader_audio_preferences.dart
+++ flutter_readium_platform_interface/lib/src/reader/reader_audio_preferences.dart
@@ -4,17 +4,22 @@ class AudioPreferences {
     this.speed,
     this.pitch,
     this.seekInterval,
+    this.controlPanelInfoType,
   });
 
   double? volume;
   double? speed;
   double? pitch;
   double? seekInterval;
+  ControlPanelInfoType? controlPanelInfoType;
 
   Map<String, dynamic> toMap() => {
         'volume': volume,
         'speed': speed,
         'pitch': pitch,
         'seekInterval': seekInterval,
+        'controlPanelInfoType': controlPanelInfoType?.toString().split('.').last,
       };
 }
+
+enum ControlPanelInfoType { standard, standardWCh, chapterTitleAuthor, chapterTitle, titleChapter }
