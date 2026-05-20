d008b3ad156da98b92d5789199b0b44533401782
Daniel Freiling <ddfreiling@gmail.com>
2026-05-20 15:50:58 +0200

    feat(example): add marionette_mcp as devDep

--- flutter_readium/example/lib/main.dart
+++ flutter_readium/example/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/foundation.dart';
+import 'package:logging/logging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:logging/logging.dart';
 import 'package:marionette_flutter/marionette_flutter.dart';
 import 'package:marionette_logging/marionette_logging.dart';
 import 'package:path_provider/path_provider.dart';
@@ -11,15 +11,9 @@ import 'pages/index.dart';
 import 'state/index.dart';
 
 Future<void> main() async {
-  // FlutterReadium.init(
-  //   androidNotificationChannelId: 'r2.navigator.flutter.audio',
-  //   androidNotificationChannelName: 'Audio playback',
-  //   downloadDebug: true,
-  // );
+  // Initialize Marionette only in debug mode
   if (kDebugMode) {
-    MarionetteBinding.ensureInitialized(
-      MarionetteConfiguration(logCollector: LoggingLogCollector()),
-    );
+    MarionetteBinding.ensureInitialized(MarionetteConfiguration(logCollector: LoggingLogCollector()));
   } else {
     WidgetsFlutterBinding.ensureInitialized();
   }
