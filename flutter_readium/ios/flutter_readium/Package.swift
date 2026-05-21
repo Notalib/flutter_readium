0c60b0573f39077938b36fb8c3903a3f601f415e
Daniel Freiling <ddfreiling@gmail.com>
2026-05-21 17:38:32 +0200

    chore: remove PromiseKit dependency
    removed upstream for years

--- flutter_readium/ios/flutter_readium/Package.swift
+++ flutter_readium/ios/flutter_readium/Package.swift
@@ -5,18 +5,16 @@
 import PackageDescription
 
 let package = Package(
-    // TODO: Update your plugin name.
     name: "flutter_readium",
     platforms: [
         .iOS("15.0"),
-        .macOS("10.15")
+        // macOS: planned but not yet implemented
     ],
     products: [
         .library(name: "flutter-readium", targets: ["flutter_readium"])
     ],
     dependencies: [
-      .package(url: "https://github.com/readium/swift-toolkit.git", .upToNextMinor(from: "3.9.0")),
-      .package(url: "https://github.com/mxcl/PromiseKit", .upToNextMinor(from: "6.8.0"))
+      .package(url: "https://github.com/readium/swift-toolkit.git", .upToNextMinor(from: "3.9.0"))
     ],
     targets: [
         .target(
@@ -26,19 +24,9 @@ let package = Package(
               .product(name: "ReadiumStreamer", package: "swift-toolkit"),
               .product(name: "ReadiumNavigator", package: "swift-toolkit"),
               .product(name: "ReadiumOPDS", package: "swift-toolkit"),
-              .product(name: "PromiseKit", package: "PromiseKit"),
             ],
             resources: [
-                // TODO: If your plugin requires a privacy manifest
-                // (e.g. if it uses any required reason APIs), update the PrivacyInfo.xcprivacy file
-                // to describe your plugin's privacy impact, and then uncomment this line.
-                // For more information, see:
-                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
                 .process("PrivacyInfo.xcprivacy"),
-
-                // TODO: If you have other resources that need to be bundled with your plugin, refer to
-                // the following instructions to add them:
-                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
             ]
         )
     ]
