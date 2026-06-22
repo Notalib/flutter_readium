f5ce4ee75eb060347adc5e1cdb30e482ac6ebd39
Morten Sjøgren <msn@nota.dk>
2026-06-22 17:44:20 +0200

    chor: build flutter web with rollup

--- flutter_readium/web/src/index.ts
+++ flutter_readium/web/src/index.ts
@@ -1,8 +1,8 @@
 /**
- * Webpack entry point.
+ * Rollup entry point.
  *
  * Imports ReadiumReader.ts which:
  *  - registers globalThis.ReadiumReader (the Dart↔JS contract)
  *  - installs the style.css
  */
-export * from "./ReadiumReader";
+import "./ReadiumReader";
