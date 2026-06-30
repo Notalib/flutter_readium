f1420955ff6de2c4db26b4ab23b128cf9040955b
Daniel Freiling <ddfreiling@gmail.com>
2026-06-30 16:10:11 +0200

    chore: use fontSize ratio in tests

--- flutter_readium/test/flutter_readium_test.dart
+++ flutter_readium/test/flutter_readium_test.dart
@@ -171,7 +171,7 @@ void main() {
 
   group('setDefaultPreferences', () {
     test('stores preferences on the platform', () {
-      final prefs = EPUBPreferences(fontSize: 150);
+      final prefs = EPUBPreferences(fontSize: 1.5);
       reader.setDefaultPreferences(prefs);
       expect(platform.defaultPreferences, prefs);
     });
